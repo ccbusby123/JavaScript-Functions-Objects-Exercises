1e62f8fc2091e1d36abbe11f6d6b5ed40d3ce946
ccbusby123 <137566179+ccbusby123@users.noreply.github.com>
2023-09-04 15:35:52 -0500

    Completed Exercise 1: Basic Function Creation

---
+++ BasicFunctionCreation.js
@@ -0,0 +1,11 @@
+//Create a function named greetUser that takes a name as a parameter and returns a greeting message.
+//The function should be able to return appropriate greetings for various names provided.
+
+let yourName = "Bob"
+
+function greetUser (name) {
+  let message = 'Hello, ' + yourName;
+  alert (message);
+  }
+  greetUser();
+  
